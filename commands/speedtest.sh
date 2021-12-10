285603e38756fb083dfd3ac65091fcbf47eb710e
jacobdjwilson <jakewil@umich.edu>
2021-12-10 13:32:05 -0500

    Update speedtest.sh
    Waits for speedtest to finish before continuing

--- commands/speedtest.sh
+++ commands/speedtest.sh
@@ -2,4 +2,4 @@
 # Note: Install speedtest-cli via Diagnostics > Command Prompt > Execute Shell Command:
 # pkg update ; pkg install -y $( pkg search speedtest-cli | awk '{ print $1 }' )
 # Traffic Totals
-/usr/local/bin/speedtest-cli --secure --simple | { echo "Attribute Value Units" ; cat ; echo ; } | sh /usr/local/bin/pfMailReport.sh -t 'Speed Test Results'
+/usr/local/bin/speedtest-cli --secure --simple | { echo "Attribute Value Units" ; cat ; echo ; } | sh /usr/local/bin/pfMailReport.sh -t 'Speed Test Results' & wait $BACK_PID
