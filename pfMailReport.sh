a33e10fe065204f5e42bd33c57ff7ab1e845f8d8
jacobdjwilson <jakewil@umich.edu>
2021-12-10 15:51:19 -0500

    Added font support

--- pfMailReport.sh
+++ pfMailReport.sh
@@ -25,19 +25,20 @@
 # under the License.
 #
 section_header_text="#fff"
-section_header_background="#001d3b"
+section_header_background="#000842"
 table_header_text="#fff"
 table_header_background="#003366"
 default_text="#000"
 even_row_color="#ddd"
 odd_row_color="#fff"
-i=0 # global counter
+font="Arial,sans-serif"
 #
-echo \<h1' 'style=\"color\:$section_header_text\;background\-color\:$section_header_background\;text\-align\:left\;font\-size\:2em\;width\:100\%\"\>$2\<\/h1\>
+i=0 # global counter
+echo \<h1' 'style=\"color\:$section_header_text\;background\-color\:$section_header_background\;text\-align\:left\;font\-size\:2em\;width\:100\%\;font\-family\:$font\;\"\>$2\<\/h1\>
 while getopts ":trl" opt; do
   case ${opt} in
     t ) # input tabulated data into HTML table with columns
-        echo \<table' 'class=\"table\"' 'style=\"width\:100\%\"\>
+        echo \<table' 'class=\"table\"' 'role\=\"presentation\"' 'style=\"width\:100\%\;border\-collapse\:collapse\;border\:0\;border\-spacing\:0\;\"\>
         echo \<thread\>
         while read line; do
             i=$(( i + 1 ))
@@ -45,13 +46,13 @@ while getopts ":trl" opt; do
             for item in $line; do
                 if [ $i -lt 2 ]
                 then
-                echo \<th' 'scope=\"col\"' 'style=\"color\:$table_header_text\;background\-color\:$table_header_background\;text\-align\:left\;\"\>$item\<\/th\>
+                echo \<th' 'scope=\"col\"' 'style=\"color\:$table_header_text\;background\-color\:$table_header_background\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$item\<\/p\>\<\/th\>
                 else
                     if [ $((i%2)) -eq 0 ]
                     then
-                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;\"\>$item\<\/td\>
+                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$item\<\/p\>\<\/td\>
                     else
-                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;\"\>$item\<\/td\>
+                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$item\<\/p\>\<\/td\>
                     fi
                 fi
             done
@@ -66,20 +67,20 @@ while getopts ":trl" opt; do
         echo \<\/table\>
     ;;
     r ) # input tabulated data into HTML table with rows
-        echo \<table' 'class=\"table\"' 'style=\"width\:100\%\"\>
+        echo \<table' 'class=\"table\"' 'role\=\"presentation\"' 'style=\"width\:100\%\;border\-collapse\:collapse\;border\:0\;border\-spacing\:0\;\"\>
         echo \<thread\>
         while read -r line; do
             i=$(( i + 1 ))
             echo \<tr\>
                 if [ $i -lt 2 ]
                 then
-                echo \<th' 'scope=\"col\"' 'style=\"color\:$table_header_text\;background\-color\:$table_header_background\;text\-align\:left\;\"\>$line\<\/th\>
+                echo \<th' 'scope=\"col\"' 'style=\"color\:$table_header_text\;background\-color\:$table_header_background\;text\-align\:left\;padding\:0\;vertical\-align\:top\;\"\>$line\<\/th\>
                 else
                     if [ $((i%2)) -eq 0 ]
                     then
-                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;\"\>$line\<\/td\>
+                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;padding\:0\;vertical\-align\:top\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$line\<\/p\>\<\/td\>
                     else
-                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;\"\>$line\<\/td\>
+                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;padding\:0\;vertical\-align\:top\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$line\<\/p\>\<\/td\>
                     fi
                 fi
             if [ $i -lt 2 ]
@@ -98,9 +99,9 @@ while getopts ":trl" opt; do
             i=$(( i + 1 ))
                 if [ $((i%2)) -eq 0 ]
                 then
-                    echo \<li' 'class=\"list\-group\-item\"' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;\"\>$line\<\/li\>
+                    echo \<li' 'class=\"list\-group\-item\"' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$line\<\/p\>\<\/li\>
                 else
-                    echo \<li' 'class=\"list\-group\-item\"' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;\"\>$line\<\/li\>
+                    echo \<li' 'class=\"list\-group\-item\"' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$line\<\/p\>\<\/li\>
                 fi
             echo \<\/ol\>
         done
