0b686be455ef925a763d6a6b489137844073dddc
jacobdjwilson <jakewil@umich.edu>
2021-12-10 11:56:24 -0500

    Update traffic_totals.sh

--- commands/traffic_totals.sh
+++ commands/traffic_totals.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Note: Install vnstat via System > Package Manager > Available Packages > Status_Traffic_Totals
+# Note: Output will look wrong until 7 days of traffic accumulates
 # Traffic Totals
 /usr/local/bin/vnstat -d -i em0 --style 3 | tail -9 | sed -e '8d' | sed 's/|//g' | sed 's/\([0-9]\) \([a-zA-Z]\)/\1\2/g' | { echo "Day RX TX Total Average" ; cat ; echo ; } | sh /usr/local/bin/pfMailReport.sh -t 'WAN Traffic Totals' && /usr/local/bin/vnstat -d -i em1 --style 3 | tail -9 | sed -e '8d' | sed 's/|//g' | sed 's/\([0-9]\) \([a-zA-Z]\)/\1\2/g' | { echo "Day RX TX Total Average" ; cat ; echo ; } | sh /usr/local/bin/pfMailReport.sh -t 'LAN Traffic Totals'
