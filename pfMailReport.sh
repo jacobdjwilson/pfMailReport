7a1c22b84f325fa546b0b2206772cbbab30649f4
Jacob Wilson <jakewil@umich.edu>
2021-12-14 11:43:39 -0500

    Formatting changes

--- pfMailReport.sh
+++ pfMailReport.sh
@@ -31,7 +31,10 @@ table_header_background="#003366"
 default_text="#000"
 even_row_color="#ddd"
 odd_row_color="#fff"
-font="Arial,sans-serif"
+font_type="Arial,sans-serif"
+font_size="16px"
+line_height="24px"
+line_margins="3px 0 3px 0"
 #
 help() {
 cat << EOF
@@ -57,7 +60,7 @@ EOF
 i=0 # global counter
 if [ -n "$1" ]; then
   if [ -n "$2" ]; then
-    echo \<h1' 'style=\"color\:$section_header_text\;background\-color\:$section_header_background\;text\-align\:left\;font\-size\:2em\;width\:100\%\;font\-family\:$font\;\"\>$2\<\/h1\>
+    echo \<h1' 'style=\"color\:$section_header_text\;background\-color\:$section_header_background\;text\-align\:left\;font\-size\:2em\;width\:100\%\;font\-family\:$font_type\;\"\>$2\<\/h1\>
   fi
 else
   echo "pfMailReport requires input from other commands. Try something like... echo log_file.log | sh /usr/local/bin/pfMailReport.sh -r 'Log Name' "
@@ -73,13 +76,13 @@ while getopts ":ctrl" opt; do
             for item in $line; do
                 if [ $i -lt 2 ]
                 then
-                echo \<th' 'scope=\"col\"' 'style=\"color\:$table_header_text\;background\-color\:$table_header_background\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$item\<\/p\>\<\/th\>
+                echo \<th' 'scope=\"col\"' 'style=\"color\:$table_header_text\;background\-color\:$table_header_background\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:$line_margins\;font\-size\:$font_size\;line\-height\:$line_height\;font\-family\:$font_type\;\"\>$item\<\/p\>\<\/th\>
                 else
                     if [ $((i%2)) -eq 0 ]
                     then
-                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$item\<\/p\>\<\/td\>
+                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:$line_margins\;font\-size\:$font_size\;line\-height\:$line_height\;font\-family\:$font_type\;\"\>$item\<\/p\>\<\/td\>
                     else
-                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$item\<\/p\>\<\/td\>
+                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:$line_margins\;font\-size\:$font_size\;line\-height\:$line_height\;font\-family\:$font_type\;\"\>$item\<\/p\>\<\/td\>
                     fi
                 fi
             done
@@ -101,13 +104,13 @@ while getopts ":ctrl" opt; do
             echo \<tr\>
                 if [ $i -lt 2 ]
                 then
-                echo \<th' 'scope=\"col\"' 'style=\"color\:$table_header_text\;background\-color\:$table_header_background\;text\-align\:left\;padding\:0\;vertical\-align\:top\;\"\>$line\<\/th\>
+                echo \<th' 'scope=\"col\"' 'style=\"color\:$table_header_text\;background\-color\:$table_header_background\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:$line_margins\;font\-size\:$font_size\;line\-height\:$line_height\;font\-family\:$font_type\;\"\>$line\<\/th\>
                 else
                     if [ $((i%2)) -eq 0 ]
                     then
-                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;padding\:0\;vertical\-align\:top\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$line\<\/p\>\<\/td\>
+                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;padding\:0\;vertical\-align\:top\;\"\>\<p' 'style\=\"margin\:$line_margins\;font\-size\:$font_size\;line\-height\:$line_height\;font\-family\:$font_type\;\"\>$line\<\/p\>\<\/td\>
                     else
-                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;padding\:0\;vertical\-align\:top\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$line\<\/p\>\<\/td\>
+                        echo \<td' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;padding\:0\;vertical\-align\:top\;\"\>\<p' 'style\=\"margin\:$line_margins\;font\-size\:$font_size\;line\-height\:$line_height\;font\-family\:$font_type\;\"\>$line\<\/p\>\<\/td\>
                     fi
                 fi
             if [ $i -lt 2 ]
@@ -126,9 +129,9 @@ while getopts ":ctrl" opt; do
             i=$(( i + 1 ))
                 if [ $((i%2)) -eq 0 ]
                 then
-                    echo \<li' 'class=\"list\-group\-item\"' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$line\<\/p\>\<\/li\>
+                    echo \<li' 'class=\"list\-group\-item\"' 'style=\"color\:$default_text\;background\-color\:$even_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:$line_margins\;font\-size\:$font_size\;line\-height\:$line_height\;font\-family\:$font_type\;\"\>$line\<\/p\>\<\/li\>
                 else
-                    echo \<li' 'class=\"list\-group\-item\"' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:0' '0' '12px' '0\;font\-size\:16px\;line\-height\:24px\;font\-family\:$font\;\"\>$line\<\/p\>\<\/li\>
+                    echo \<li' 'class=\"list\-group\-item\"' 'style=\"color\:$default_text\;background\-color\:$odd_row_color\;text\-align\:left\;\"\>\<p' 'style\=\"margin\:$line_margins\;font\-size\:$font_size\;line\-height\:$line_height\;font\-family\:$font_type\;\"\>$line\<\/p\>\<\/li\>
                 fi
             echo \<\/ol\>
         done
