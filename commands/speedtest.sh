cc4fb7e21f7a9faf1727abb37f8fdcfabc64c614
Jacob Wilson <jacobdjwilson@gmail.com>
2022-02-28 10:12:41 -0500

    Update speedtest.sh

--- commands/speedtest.sh
+++ commands/speedtest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Note: Install speedtest-cli via Diagnostics > Command Prompt > Execute Shell Command:
 # pkg update ; pkg install -y $( pkg search speedtest-cli | awk '{ print $1 }' )
-# Traffic Totals
+# Speed Test
 /usr/local/bin/speedtest-cli --secure --simple | { echo "Attribute Value Units" ; cat ; echo ; } | sh /usr/local/bin/pfMailReport.sh -t 'Speed Test Results'
