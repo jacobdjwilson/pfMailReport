33b043335904c7729766aab25bd77af15f6cadad
Jacob Wilson <jacobdjwilson@gmail.com>
2022-04-04 09:25:11 -0400

    Additional CRON guidance

--- commands/speedtest_full.sh
+++ commands/speedtest_full.sh
@@ -3,3 +3,9 @@
 # pkg update ; pkg install -y $( pkg search speedtest-cli | awk '{ print $1 }' )
 # Speed Test
 /usr/local/bin/speedtest-cli --secure --share | sed -e '1d;3d;4d;6d;8d' | { date ; cat ; echo ; } | sh /usr/local/bin/pfMailReport.sh -r 'Speed Test Results'
+#
+# If you're having trouble with the email sending before the speed test completes try this
+# Services > CRON > Add
+# /usr/local/bin/speedtest-cli --secure --share | sed -e '1d;3d;4d;6d;8d' > /tmp/speedtest.out
+# Then change the email report to read from the tmp .out file
+# cat /tmp/speedtest.out | { date ; cat ; echo ; } | sh /usr/local/bin/pfMailReport.sh -r 'Speed Test Results'
